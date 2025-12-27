ec46b53a08b0bdd5550d5681e58ddbf55c43be3f
CristianM <mija.cristian3101@gmail.com>
2025-12-27 13:47:32 +0200

    feat:add student-confirmation

--- src/main/java/pixel/academy/thymeleaf/app/controller/StudentController.java
+++ src/main/java/pixel/academy/thymeleaf/app/controller/StudentController.java
@@ -3,6 +3,8 @@ package pixel.academy.thymeleaf.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pixel.academy.thymeleaf.app.model.Student;
 
 @Controller
@@ -12,13 +14,22 @@ public class StudentController {
     public String showFrom(Model theModel) {
 
         //create a nre sudent object
-        Student stheStudent = new Student();
+        Student theStudent = new Student();
 
         //add st object to the model
 
-        theModel.addAttribute("student" , stheStudent);
+        theModel.addAttribute("student" , theStudent);
 
         return "student-form";
+
+    }
+    @PostMapping
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        //log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
 
     }
 
